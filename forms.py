29974797efe1b76aaf81bea48bd69f4ecfe41217
Stefania <stefaniamartinezpro@gmail.com>
2026-03-07 09:27:49 -0600

    Maestros 07/03/2026

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from wtforms import Form
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, TextAreaField, SelectField, HiddenField
 from wtforms import EmailField
 from wtforms import validators
 
@@ -23,3 +23,46 @@ class UserForm(Form):
     telefono=StringField("telefono", [
         validators.DataRequired(message="El telefono es requerido")
     ])
+
+class MaestroForm(Form):
+    matricula = IntegerField("matricula", [
+        validators.DataRequired(message="La matricuala es requerida")
+    ])
+    nombre=StringField("nombre", [
+        validators.DataRequired(message="El nombre es requerido"),
+        validators.length(min=4,max=20, message='requiere min=4 max=20')
+    ])
+    apellidos=StringField("apellidos", [
+        validators.DataRequired(message="El apellido es requerido")
+    ])
+    especialidad = StringField("especialidad", [
+        validators.DataRequired(message="La especialidad es requerido")
+    ])
+    email=EmailField("email", [
+        validators.Email(message="El email es requerido"),
+        validators.Email(message='Ingrese un email valido')
+    ])
+
+class CursoForm(Form):
+    id = IntegerField("id")
+
+    nombre = StringField("nombre", [
+        validators.DataRequired(message="El nombre del curso es requerido"),
+        validators.length(min=4, max=30, message="requiere min=4 max=30")
+    ])
+
+    descripcion = TextAreaField("descripcion", [
+        validators.DataRequired(message="La descripción es requerida"),
+        validators.length(min=10, max=200, message="requiere min=10 max=200")
+    ])
+
+    maestro_id = SelectField("maestro_id",
+        coerce=int,
+        validators=[
+            validators.DataRequired(message="El maestro es requerido")
+        ]
+    )
+
+class InscripcionForm(Form):
+    curso_id = HiddenField("curso_id")
+    alumno_id = SelectField("alumno_id", coerce=int)
--- maestros/routes.py
+++ maestros/routes.py
@@ -7,9 +7,9 @@ from config import DevelopmentConfig
 from flask import g
 import forms
 from flask_migrate import Migrate
-from maestros.routes import maestros,maestros
+#from maestros.routes import maestros,maestros
 from models import db
-from models import Alumnos, Maestros
+from models import Alumnos, Maestros, Curso
 
 @maestros.route('/perfil/<nombre>')
 def perfil(nombre):
@@ -21,3 +21,84 @@ def index():
     create_form=forms.UserForm(request.form)
     maestros=Maestros.query.all()
     return render_template("maestros/listadoMaes.html", form=create_form, maestros=maestros)
+
+@maestros.route("/nuevo", methods=['GET', 'POST'])
+def nuevo():
+    create_form = forms.MaestroForm(request.form)
+    if request.method == 'POST':
+        if create_form.validate():
+            nuevo_maestro = Maestros(
+                matricula=create_form.matricula.data,
+                nombre=create_form.nombre.data,
+                apellidos=create_form.apellidos.data,
+                especialidad=create_form.especialidad.data,
+                email=create_form.email.data
+            )
+            db.session.add(nuevo_maestro)
+            db.session.commit()
+            return redirect("/maestros")
+    return render_template("maestros/Maestros.html", form=create_form)
+
+@maestros.route("/modificar", methods=['GET','POST'])
+def modificar():
+	create_form = forms.MaestroForm(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		# select * from maestros where matricula == id
+		maes1 = db.session.query(Maestros).filter(Maestros.matricula == id).first()
+		create_form.matricula.data = request.args.get('id')
+		create_form.nombre.data = maes1.nombre
+		create_form.apellidos.data = maes1.apellidos
+		create_form.especialidad.data = maes1.especialidad
+		create_form.email.data = maes1.email
+
+	if request.method == 'POST':
+		id = create_form.matricula.data
+		# select * from maestros where matricula == id
+		maes1 = db.session.query(Maestros).filter(Maestros.matricula == id).first()
+		maes1.matricula = id
+		maes1.nombre = create_form.nombre.data
+		maes1.apellidos = create_form.apellidos.data
+		maes1.especialidad = create_form.especialidad.data
+		maes1.email = create_form.email.data
+
+		# db.session.add(maes1)
+		db.session.commit()
+		return redirect(url_for('maestros.index'))
+	return render_template("maestros/modificarMaes.html", form=create_form)
+
+@maestros.route("/detalles", methods=['GET','POST'])
+def detalles():
+	create_form=forms.MaestroForm(request.form)
+	if request.method=='GET':
+		id=request.args.get('id')
+		# select * from alumnos where id == id
+		maes1=db.session.query(Maestros).filter(Maestros.matricula==id).first()
+		id=request.args.get('id')
+		nombre=maes1.nombre
+		apellidos=maes1.apellidos
+		email=maes1.email
+		cursos = db.session.query(Curso).filter(Curso.maestro_id == id).all()
+	return render_template('maestros/detallesMaes.html', id=id,nombre=nombre,apellidos=apellidos,email=email, cursos=cursos)
+
+@maestros.route("/eliminar", methods=['GET','POST'])
+def eliminar():
+	create_form = forms.MaestroForm(request.form)
+	if request.method == 'GET':
+		id = request.args.get('id')
+		# select * from maestros where matricula == id
+		maes1 = db.session.query(Maestros).filter(Maestros.matricula == id).first()
+		create_form.matricula.data = request.args.get('id')
+		create_form.nombre.data = maes1.nombre
+		create_form.apellidos.data = maes1.apellidos
+		create_form.especialidad.data = maes1.especialidad
+		create_form.email.data = maes1.email
+
+	if request.method == 'POST':
+		id = create_form.matricula.data
+		# select * from maestros where matricula == id
+		maes = Maestros.query.get(id)
+		db.session.delete(maes)
+		db.session.commit()
+		return redirect(url_for('maestros.index'))
+	return render_template("maestros/eliminarMaes.html", form=create_form)
